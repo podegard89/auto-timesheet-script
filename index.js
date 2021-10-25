1b8d4b4c25c20e6253f1ed77e38a44311bf2a0ef
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-10-25 00:17:26 -0500

    Created full MVP of banner crawler

--- index.js
+++ index.js
@@ -3,61 +3,58 @@ const secrets = require('./secrets');
 const bannerURL = require('./url.json').bannerURL;
 const Sheet = require('./sheet');
 
-// const convertDateFormat = (dateString) => {
-//     const monthNames = [
-//         "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-//         "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
-//     ];
-//     const splitDate = dateString.split('/');
-//     const monthIndex = parseInt(splitDate[0]) - 1;
-//     return `${splitDate[1]}-${monthNames[monthIndex]}-${splitDate[2]}`;
-// }
-
 (async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(bannerURL);
+
+    //login
+    const loginFormElements = await page.$$('input');
+    await loginFormElements[0].type(secrets.USERNAME);
+    await loginFormElements[1].type(secrets.PASSWORD);
+    await loginFormElements[2].click();
+
+    const waitThenClick = async (selector) => {
+        await page.waitForSelector(selector);
+        const element = await page.$(selector);
+        await element.click();
+    }
+
+    const selectors = [
+        '.taboff', '[name="GeneralOpenFolder"]',
+        '[value="Time Sheet"]'
+    ]
+
+    // crawl to banner time sheet
+    for (let s of selectors) {
+        await waitThenClick(s);
+    }
+
+    // get rows from Time sheet for date/hour data
     const sheet = new Sheet();
     await sheet.load();
-    const timeSheetLogs = await sheet.getRows(0);
-    const shiftDate = convertDateFormat(timeSheetLogs[0].currDate);
-    const shiftHours = timeSheetLogs[0].hours;
+    // currently only grabs last 5, will need to be updated to 10
+    const timeSheetRows = (await sheet.getRows(0)).slice(1).slice(-10);
 
-    // const browser = await puppeteer.launch({ headless: false });
-    // const page = await browser.newPage();
-    // await page.goto(bannerURL);
+    // then loop through the data and enter it into banner time sheet
+    for (const [index, row] of timeSheetRows.entries()) {
+        if (index === 5) {
+            await waitThenClick('[value="Next"]');
+        }
 
-    // //login
-    // const loginFormElements = await page.$$('input');
-    // await loginFormElements[0].type(secrets.USERNAME);
-    // await loginFormElements[1].type(secrets.PASSWORD);
-    // await loginFormElements[2].click();
+        const shiftDate = row.currDate;
+        const shiftHours = row.hours;
+        console.log({shiftDate, shiftHours});
 
-    // //navigate to employee tab
-    // await page.waitForSelector('.taboff');
-    // const employeeTab = await page.$('.taboff');
-    // await employeeTab.click();
+        await waitThenClick(`[title="Enter Hours for 015 Hourly Pay for ${shiftDate}"]`);
 
-    // //navigate to submit/approve timesheets
-    // await page.waitForSelector('[name="GeneralOpenFolder"]');
-    // const submitNav = await page.$('[name="GeneralOpenFolder"]');
-    // await submitNav.click();
+        await page.waitForSelector('input[name="Hours"]');
+        const hoursInput = await page.$('input[name="Hours"]');
+        await hoursInput.click({ clickCount: 3 });
+        await hoursInput.type(shiftHours);
+        const saveButton = await page.$('input[value="Save"]');
+        await saveButton.click();
+    }
 
-    // await page.waitForSelector('[value="Time Sheet"]');
-    // const timeSheetButton = await page.$('[value="Time Sheet"]');
-    // await timeSheetButton.click();
-
-    // //get rows from Time sheet for date/hour data
-    
-
-    // //select one enter hours cell and input time
-    // await page.waitForSelector('[title="Enter Hours for 015 Hourly Pay for Sunday 17-OCT-2021"]');
-    // const enterHoursButton = await page.$('[title="Enter Hours for 015 Hourly Pay for Sunday 17-OCT-2021"]');
-    // await enterHoursButton.click();
-
-    // await page.waitForSelector('input[name="Hours"]');
-
-    // const hoursInput = await page.$('input[name="Hours"]');
-    // await hoursInput.type('');
-    // const saveButton = await page.$('input[value="Save"]');
-    // // await saveButton.click();
-
-    // // await browser.close();
+    await browser.close();
 })()
