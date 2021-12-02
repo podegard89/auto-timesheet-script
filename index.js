f4536a58866a2ccb73717f1a57e0bbb8fb1cb00b
Pearce Odegard <pearceodegard@gmail.com>
2021-12-02 03:23:17 -0600

    Added try/catch/finally

--- index.js
+++ index.js
@@ -3,61 +3,71 @@ const secrets = require('./secrets');
 const Sheet = require('./sheet');
 
 (async () => {
+
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(secrets.bannerUrl);
+    try {
+        //login
+        const loginFormElements = await page.$$('input');
+        await loginFormElements[0].type(secrets.USERNAME);
+        await loginFormElements[1].type(secrets.PASSWORD);
+        await loginFormElements[2].click();
 
-    //login
-    const loginFormElements = await page.$$('input');
-    await loginFormElements[0].type(secrets.USERNAME);
-    await loginFormElements[1].type(secrets.PASSWORD);
-    await loginFormElements[2].click();
-
-    const waitThenClick = async (selector) => {
-        await page.waitForSelector(selector);
-        const element = await page.$(selector);
-        await element.click();
-    }
-
-    // get rows from selector sheet
-    const sheet = new Sheet();
-    await sheet.load();
-
-    const selectors = await sheet.getRows(1);
-
-    // crawl to banner time sheet
-    for (let row of selectors) {
-        await waitThenClick(row.selector);
-    }
-
-    // get number of days in last pay period from sheet 3
-    const payPeriods = await sheet.getRows(2);
-    const payPeriod = payPeriods[payPeriods.length - 1].payPeriod;
-    console.log(payPeriod)
-    // grabs last payPeriod rows from time sheet
-    const timeSheetRows = (await sheet.getRows(0)).slice(1).slice(-payPeriod);
-    console.log(timeSheetRows.length);
-    // then loop through the data and enter it into banner time sheet
-    for (const [index, row] of timeSheetRows.entries()) {
-        if (index === 5) {
-            await waitThenClick('[value="Next"]');
+        const waitThenClick = async (selector) => {
+            await page.waitForSelector(selector);
+            const element = await page.$(selector);
+            await element.click();
         }
 
-        const shiftDate = row.currDate;
-        const shiftHours = row.hours;
-        console.log({shiftDate, shiftHours});
+        // get rows from selector sheet
+        const sheet = new Sheet();
+        await sheet.load();
 
-        await waitThenClick(`[title="Enter Hours for 015 Hourly Pay for ${shiftDate}"]`);
+        const selectors = await sheet.getRows(1);
 
-        await page.waitForSelector('input[name="Hours"]');
-        const hoursInput = await page.$('input[name="Hours"]');
-        await hoursInput.click({ clickCount: 3 });
-        await hoursInput.type(shiftHours);
-        const saveButton = await page.$('input[value="Save"]');
-        await saveButton.click();
+        // crawl to banner time sheet
+        for (let row of selectors) {
+            await waitThenClick(row.selector);
+        }
+
+        // get number of days in last pay period from sheet 3
+        const payPeriods = await sheet.getRows(2);
+        const payPeriod = payPeriods[payPeriods.length - 1].payPeriod;
+        console.log(payPeriod)
+        // grabs last payPeriod rows from time sheet
+        const timeSheetRows = (await sheet.getRows(0)).slice(1).slice(-payPeriod);
+        console.log(timeSheetRows.length);
+        // then loop through the data and enter it into banner time sheet
+        for (const [index, row] of timeSheetRows.entries()) {
+            if (index === 5) {
+                await waitThenClick('[value="Next"]');
+            }
+
+            const shiftDate = row.currDate;
+            const shiftHours = row.hours;
+            console.log({ shiftDate, shiftHours });
+
+            await waitThenClick(`[title="Enter Hours for 015 Hourly Pay for ${shiftDate}"]`);
+
+            await page.waitForSelector('input[name="Hours"]');
+            const hoursInput = await page.$('input[name="Hours"]');
+            await hoursInput.click({ clickCount: 3 });
+            await hoursInput.type(shiftHours);
+            const saveButton = await page.$('input[value="Save"]');
+            await saveButton.click();
+        }
+
+        await browser.close();
+        console.log("It worked!");
+    } catch (error) {
+        console.log("It didn't work :(");
+
+        console.log(error);
+    } finally {
+        // I am not automatically closing the browser for now so I can confirm
+        // everything is working and manually submit hours for approval.
+        await browser.close();
     }
 
-    // I am not automatically closing the browser for now so I can confirm
-    // everything is working and manually submit hours for approval.
-    // await browser.close();
 })()
